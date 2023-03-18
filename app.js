724b2ab4c952960bb2320095866af8e4ff13a50f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-18 14:17:26 +0600

    Dynamically display loaded data show  on  website

--- app.js
+++ app.js
@@ -6,8 +6,31 @@ const loadProduct = async () => {
 
 }
 const displayProduct = data => {
-    console.log(data);
-    
+    // console.log(data);
+    const cards = document.getElementById('cards');
+    data.forEach((product) => {
+        console.log(product);
+        const card = document.createElement("div");
+        card.classList.add("card", "m-2");
+
+
+        card.innerHTML = `
+        <div class="product-img-container">
+          <img
+            class="product-img"
+            src=${product.image}
+            alt=""
+          />
+        </div>
+        <h3>${product.name}</h3>
+        <p>The Widget 3000 is the latest and greatest in widget</p>
+        <div class="priceAndButtons">
+          <h2 class="text-primary">$${product.price}</h2>
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+          `;
+            cards.appendChild(card);
+    })
 }
 
 loadProduct();
