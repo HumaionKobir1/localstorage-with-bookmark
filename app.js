6fe49cafc9e8b7d81ab0c7d60f5718618465b78f
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-18 20:55:45 +0600

    added => sweetalert when click the bookmark btn and set the data local storage

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const displayProduct = data => {
     // console.log(data);
     const cards = document.getElementById('cards');
     data.forEach((product) => {
-        console.log(product);
+        // console.log(product);
         const card = document.createElement("div");
         card.classList.add("card", "m-2");
 
 
         card.innerHTML = `
+        <div class="bookmark-icon">
+            <i class="fa-solid fa-bookmark"></i>
+            <i onclick="handleBookmark('${product.name}', '${product.id}', '${product.price}')" class="fa-regular fa-bookmark"></i>
+        </div>
+
         <div class="product-img-container">
           <img
             class="product-img"
@@ -31,6 +36,39 @@ const displayProduct = data => {
           `;
             cards.appendChild(card);
     })
-}
+};
+
+// handle book mark
+const handleBookmark = (name, id, price) => {
+    const previousBookmark = JSON.parse(localStorage.getItem('bookmark'));
+    let bookmark = [];
+    const product = {name, id, price, bookmark: true};
+
+    if(previousBookmark){
+        const isThisProductMarked = previousBookmark.find((pd) => pd.id == id);
+        
+        if(isThisProductMarked){
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "already bookmarked",
+                footer: '<a herf=""> Why do I have this issue?</a>',
+            });
+        }
+        else{
+            bookmark.push(...previousBookmark, product);
+            localStorage.setItem("bookmark", JSON.stringify(bookmark));
+            // console.log(bookmark);
+
+        }
+    }
+
+    else{
+        bookmark.push(product);
+        localStorage.setItem("bookmark", JSON.stringify(bookmark));
+        
+    }
+    // console.log(previousBookmark);
+};
 
 loadProduct();
